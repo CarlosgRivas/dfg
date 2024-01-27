1b9f12019d11ff3ade5d74c9f85ebb9020425942
CarlosgRivas <cgrivasguevara@gmail.com>
2024-01-27 20:21:15 -0300

    mostrar registro existentes

--- mostrar.php
+++ mostrar.php
@@ -12,7 +12,25 @@ require ('sesion.php');
 </head>
 
 <body>
-	
+	<p>Bienvenido: <?php echo $_SESSION['usuario'];?><br/></p>
+	<a href="salir.php?salir=si">CERRAR SESIÓN</a>
+	<h1 align="center">Registros existentes</h1>
+	<br><br>
+	<?php
+		require ('conexion.php');
+		$consulta = "SELECT * FROM registros";
+		$ejecutar = mysql_query($consulta, $conexion) or die('Problemas al buscar info </br>' . mysql_errno($conexion) . ": " . mysql_error($conexion));
+		echo '<table><tr><th>Rut</th><th>Nombre</th><th>Apellido</th><th>Email</th></tr>';
+		while ($user = mysql_fetch_array($ejecutar)) {
+			echo '<tr>';
+			echo "<td>{$user['rut']}</td>";
+			echo "<td>{$user['nombre']}</td>";
+			echo "<td>{$user['apellido']}</td>";
+			echo "<td>{$user['email']}</td>";
+			echo '</tr>';
+		}
+		echo '</table>';
+	?>
 
 </body>
 </html>
