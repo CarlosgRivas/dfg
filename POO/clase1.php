fa4dc29d572866a2dacf1776bb5996202684c95a
CarlosgRivas <cgrivasguevara@gmail.com>
2024-02-03 10:31:20 -0300

    1ra clase

--- POO/clase1.php
+++ POO/clase1.php
@@ -1,3 +1,27 @@
 <?php 
-    echo 'HI!';
+    class Persona {
+        public $edad;
+        public function saludar() {
+            echo 'Hi!';
+        }
+
+        public function decirEdad() {
+            echo "Tengo " . $this->edad . " años";
+        }
+    }
+
+    /* antes del constructor
+    $p1 = new Persona();
+    $p1->edad = 17;
+    echo $p1->edad . "<br/>";
+    $p1->saludar();
+    echo ", ";
+    $p1->decirEdad();
+    echo "<br/>";
+    $p2 = new Persona();
+    $p2->edad = 15;
+    $p2->saludar();
+    echo ", ";
+    $p2->decirEdad();
+    */
 ?>
