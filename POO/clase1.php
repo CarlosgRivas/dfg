5febf9ca9343718767e773f9fc8d217c973e44e4
CarlosgRivas <cgrivasguevara@gmail.com>
2024-02-20 18:34:26 -0300

    herencia

--- POO/clase1.php
+++ POO/clase1.php
@@ -30,6 +30,18 @@
         }
     }
 
+    class Persona3 extends Persona2 {
+        public $color;
+        public function __construct($nombre, $edad=null, $color=null) {
+            parent::__construct($nombre, $edad);
+            $this->color = $color;
+        }
+
+        public function colorFav() {
+            echo "Mi color favorito es el {$this->color}";
+        }
+    }
+
     $p1 = new Persona1();
     $p1->edad = 17;
     echo $p1->edad . "<br/>";
@@ -37,6 +49,7 @@
     echo ", ";
     $p1->decirEdad();
     echo "<br/>";
+
     $p2 = new Persona1();
     $p2->edad = 15;
     $p2->saludar();
@@ -48,5 +61,13 @@
     $p3->saludar();
     echo ". ";
     $p3->decirEdad();
+    echo "<br/>";
+
+    $p4 = new Persona3('Gabriel', 17, 'Azul');
+    $p4->saludar();
+    echo ". ";
+    $p4->decirEdad();
+    echo ". ";
+    $p4->colorFav();
     echo "<br/>"
 ?>
