cd4e046923d1f9140127669e2570f405ec36e33a
CarlosgRivas <cgrivasguevara@gmail.com>
2024-02-20 17:27:20 -0300

    constructor

--- POO/clase1.php
+++ POO/clase1.php
@@ -1,5 +1,5 @@
 <?php 
-    class Persona {
+    class Persona1 {
         public $edad;
         public function saludar() {
             echo 'Hi!';
@@ -10,18 +10,43 @@
         }
     }
 
-    /* antes del constructor
-    $p1 = new Persona();
+    class Persona2 {
+        public $nombre;
+        public $edad;
+        public function __construct($nombre, $edad=null) {
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+        }
+        public function saludar() {
+            echo 'Hi!, soy '. $this->nombre .'. Un gusto :)';
+        }
+
+        public function decirEdad() {
+            if ($this->edad == null) {
+                echo 'No se cual es mi edad XD';
+            } else {
+                echo "Tengo " . $this->edad . " años";
+            }
+        }
+    }
+
+    $p1 = new Persona1();
     $p1->edad = 17;
     echo $p1->edad . "<br/>";
     $p1->saludar();
     echo ", ";
     $p1->decirEdad();
     echo "<br/>";
-    $p2 = new Persona();
+    $p2 = new Persona1();
     $p2->edad = 15;
     $p2->saludar();
     echo ", ";
     $p2->decirEdad();
-    */
+    echo "<br/>";
+
+    $p3 = new Persona2('Carlos');
+    $p3->saludar();
+    echo ". ";
+    $p3->decirEdad();
+    echo "<br/>"
 ?>
