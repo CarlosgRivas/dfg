bf9ef350e31d18c8b7956cb5c723e23f0c52c834
CarlosgRivas <cgrivasguevara@gmail.com>
2024-12-23 18:55:50 -0300

    Mostrar

--- funciones/mostrar.php
+++ funciones/mostrar.php
@@ -4,13 +4,34 @@
 <head>
 	<meta charset="UTF-8"/>
 	<title>mostrar PERSONAL</title>
-	<link type="text/css" href="estilo.css" rel="stylesheet">
+	<link type="text/css" href="./../estilo.css" rel="stylesheet">
 
 </head>
 
 <body>
-	<?php 
-		require('sesion.php');
-	?>
+	<table>
+		<tr>
+			<th>Rut</th>
+			<th>Nombre completo</th>
+			<th>Email</th>
+		</tr>
+	
+		<?php 
+			require('sesion.php');
+			require('./../conexion.php');
+
+			$sql = 'SELECT rut, CONCAT(nombre, apellido) AS nombre_completo, email FROM users';
+			$result = ($db->query($sql))->fetchAll(PDO::FETCH_ASSOC);
+
+			
+			foreach ( $result as $user ) {
+				echo '<tr>';
+				echo '<td>' . $user['rut'] . '</td>';
+				echo '<td>' . $user['nombre_completo'] . '</td>';
+				echo '<td>' . $user['email'] . '</td>';
+				echo '</tr>';
+			}
+		?>
+	</table>
 </body>
 </html>
