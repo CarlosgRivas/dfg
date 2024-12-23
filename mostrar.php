fdbf0fe94da9671d1c16545d2468ea7bdecbb086
CarlosgRivas <cgrivasguevara@gmail.com>
2024-12-23 17:12:42 -0300

    add funciones

---
+++ mostrar.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>formulario mostar PERSONAL</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
+
+</head>
+
+<body>
+	
+
+</body>
+</html>
